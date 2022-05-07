adb86081b3fdfcb29407833d6be863db196d7368
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-05-06 22:36:43 -0300

    feat: get e post do mural

--- src/controllers/muralController.js
+++ src/controllers/muralController.js
@@ -1,27 +1,32 @@
+const { uid } = require("uid");
 
 let fakelist = [
    {
       id: 1,
       titulo: "Jogar bola",
-      description: "Sair de casa e ver o sol pra jogar uma bolinha e tomar uma cervejinha!"
+      description:
+         "Sair de casa e ver o sol pra jogar uma bolinha e tomar uma cervejinha!",
    },
    {
       id: 2,
       title: "Jogar WOW",
-      description: "Lutar pela Horda e destruição da Ally!"
+      description: "Lutar pela Horda e destruição da Ally!",
    },
-   {
-      id: 3,
-      titulo: "Um título",
-      description: "Uma mensagem aleatória!!!"
-   },
-]
+];
 
 const muralController = {
    list(req, res) {
       res.status(200).json(fakelist);
    },
-   newPost(req, res) {},
+
+   newPost(req, res) {
+      const { title, description } = req.body;
+      const id = uid(24);
+
+      fakelist.push({id, title, description});
+
+      res.status(201).json(fakelist);
+   },
 };
 
 module.exports = muralController;
